e90812473342b823e2b59ceec8bc04200925d8c8
wildan3105 <winistein@gmail.com>
2022-07-05 17:46:01 +0700

    refactor(calculation-lib): convert scientific notation into constant

--- libs/calculation.ts
+++ libs/calculation.ts
@@ -1,7 +1,10 @@
+const milisIntoHours = 36e5;
+
+
 export async function calculateDateDiffsInHours(
   upcomingDate: never,
   nowDate: never
 ): Promise<number> {
-  const diffInHours = Number(((upcomingDate - nowDate) / 36e5).toFixed(0));
+  const diffInHours = Number(((upcomingDate - nowDate) / milisIntoHours).toFixed(0));
   return diffInHours;
 }
