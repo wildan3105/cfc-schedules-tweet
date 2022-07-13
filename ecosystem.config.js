6cd05e31ecf828537cc125c67ce875050abd69a8
wildan3105 <winistein@gmail.com>
2022-07-13 09:06:33 +0700

    update ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,12 +30,6 @@ module.exports = {
 
   deploy : {
     production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': 'npm install & npm run build'
     }
