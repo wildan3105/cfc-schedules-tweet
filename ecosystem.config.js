f86486c18ad980b985123edc965d3d99dbc9f681
wildan3105 <winistein@gmail.com>
2022-07-13 09:05:46 +0700

    configure env variables for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,18 +4,27 @@ module.exports = {
   {
     name: 'subscriber',
     script: 'build/events/sub.js',
+    env_production: {
+      NODE_ENV: "production",
+    }
   },
   {
     name: 'match-reader-jobs',
     script: 'build/jobs/match-reader.js',
     cron_restart: '*/1 * * * *', // hourly
-    autorestart: false
+    autorestart: false,
+    env_production: {
+      NODE_ENV: "production",
+    }
   },
   {
     name: 'match-fetcher-jobs',
     script: 'build/jobs/match-fetcher.js',
     cron_restart: '0 0 */2 * *', // At 00:00 on every 2nd day-of-month
-    autorestart: false
+    autorestart: false,
+    env_production: {
+      NODE_ENV: "production",
+    }
   }
 ],
 
@@ -28,7 +37,7 @@ module.exports = {
       path : 'DESTINATION_PATH',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+      'pre-setup': 'npm install & npm run build'
     }
   }
 };
