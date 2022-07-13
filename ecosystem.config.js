9de636c06c04f4a9c88385331486699c01d320d8
wildan3105 <winistein@gmail.com>
2022-07-13 16:45:10 +0700

    fix: remove unused Procfile

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -31,7 +31,7 @@ module.exports = {
   deploy : {
     production : {
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production && pm2 logs all',
-      'pre-setup': 'npm install & npm run build'
+      'pre-setup': 'npm install && npm run build'
     }
   }
 };
