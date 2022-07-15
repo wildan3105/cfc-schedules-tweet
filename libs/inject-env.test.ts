7785b801354ce27286d2e91d6b0c29bd7fe966d6
wildan3105 <winistein@gmail.com>
2022-07-15 10:26:25 +0700

    fix: remove test.only

--- libs/inject-env.test.ts
+++ libs/inject-env.test.ts
@@ -1,13 +1,13 @@
 import { injectEnv } from "./inject-env";
 
 describe("test to ensure environment variables are injected correctly", () => {
-    test.only("test injectEnv when path is not provided", () => {
+    test("test injectEnv when path is not provided", () => {
       const env = injectEnv();
       expect(typeof env).toBe("object");
       expect(env).toBeDefined();
     });
 
-    test.only("test injectEnv when path is provided", () => {
+    test("test injectEnv when path is provided", () => {
       const env = injectEnv("./.env.example");
       expect(typeof env).toBe("object");
       expect(env).toHaveProperty("parsed");
