6b9e002d87d9694fb5d81607ba4905820e56fa97
Wildan S. Nahar <winistein@gmail.com>
2024-05-29 14:17:55 +0700

    Test/add test for oauth helper (#100)
    * test: init test for oauth
    
    * npm run format
    
    * test(oauth): only expect the length of the header to be the same

---
+++ modules/oauth.test.ts
@@ -0,0 +1,61 @@
+import oauth1a = require("oauth-1.0a");
+import { Oauth1Helper } from "./oauth";
+import { createHmac } from "crypto";
+import { Content } from "../interfaces/tweet";
+
+jest.mock("crypto", () => ({
+  createHmac: jest.fn().mockReturnValue({
+    update: jest.fn().mockReturnThis(),
+    digest: jest.fn().mockReturnValue("mock-digest")
+  })
+}));
+
+describe("Oauth1Helper", () => {
+  const originalEnv = process.env;
+  const request = {
+    url: "https://api.twitter.com/2/tweets",
+    method: "POST",
+    body: {
+      text: "Hello world!"
+    } as Content
+  };
+
+  beforeAll(() => {
+    process.env = {
+      ...originalEnv,
+      API_KEY: "mockApiKey",
+      API_SECRET_KEY: "mockApiSecretKey",
+      ACCESS_TOKEN: "mockAccessToken",
+      ACCESS_TOKEN_SECRET: "mockAccessTokenSecret"
+    };
+  });
+
+  afterAll(() => {
+    process.env = originalEnv;
+  });
+
+  it("should generate correct OAuth header for a request", () => {
+    const header = Oauth1Helper.getAuthHeaderForRequest(request);
+
+    const oauth = new oauth1a({
+      consumer: {
+        key: "mockApiKey",
+        secret: "mockApiSecretKey"
+      },
+      signature_method: "HMAC-SHA1",
+      hash_function(base_string: any, key: any) {
+        return createHmac("sha1", key).update(base_string).digest("base64");
+      }
+    });
+
+    const expectedAuthorization = oauth.authorize(request, {
+      key: "mockAccessToken",
+      secret: "mockAccessTokenSecret"
+    });
+
+    const expectedHeader = oauth.toHeader(expectedAuthorization);
+
+    expect(header.Authorization.length).toEqual(expectedHeader.Authorization.length);
+    expect(createHmac).toHaveBeenCalledWith("sha1", "mockApiSecretKey&mockAccessTokenSecret");
+  });
+});
