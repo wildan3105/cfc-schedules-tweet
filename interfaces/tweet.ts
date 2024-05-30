f539051dd578472570e456e4e0368699683d2ad1
Wildan S. Nahar <winistein@gmail.com>
2024-05-30 15:07:05 +0700

    refactor(subscriber): remove extra checking of hours_to_match (#110)

--- interfaces/tweet.ts
+++ interfaces/tweet.ts
@@ -4,3 +4,13 @@ export interface Content {
   text: string;
   reply_settings?: ReplySettings;
 }
+
+export interface ITweet {
+  hours_to_match: number;
+  message: {
+    stadium: string;
+    participants: string;
+    match_time: Date;
+    tournament: string;
+  };
+}
